59f6c49a4cb3763f198978fb42223485d5879489
David <david.rubin95@gmail.com>
2019-06-04 17:08:54 +0200

    Naloga6 graphs units, new coefficients

--- Naloga6/filter.py
+++ Naloga6/filter.py
@@ -9,16 +9,16 @@ from zplane import zplane
 from scipy import signal
 
 
-b = np.array([8, 3, 1/9, 0, 1, 7/11, 16/19])  # b koeficienti filtra (nicle)
-a = np.array([3/4, 1, 1/3, 6/13, 25/29, 3, 3/8])  # a koeficient filtra (poli)
+b = np.array([2, 1, -1/9, 0, 1, -7/11, 2])  # b koeficienti filtra (nicle)
+a = np.array([3/4, -1, 2/3, -6/13, -25/29, -3, 3/8])  # a koeficient filtra (poli)
 
 
-def plot_freq_response(h, filename, fignum=2):
+def plot_freq_response(h, w, filename, fignum=2):
     """ Shrani png frekvencnega odziva v datoteko"""
     plt.figure(fignum)
-    plt.plot(np.real(h), label='Frekvenčni odziv')
-    plt.xlabel('Frekvenca')
-    plt.ylabel('Amplituda')
+    plt.plot(w, 20 * np.log10(abs(h)), label='Frekvenčni odziv')
+    plt.xlabel('Normalizirana frekvenca [x pi rad/sample]')
+    plt.ylabel('Amplituda [dB]')
     plt.savefig(filename)
 
 
@@ -39,7 +39,8 @@ zeros, poles, _ = zplane(b, a, 'images/zplane_before.png')
 # w ... The normalized frequencies at which h was computed, in radians/sample.
 # h ... The frequency response
 w, h = signal.freqz(b, a)
-plot_freq_response(h, 'images/freq-response_before.png', fignum=2)
+w = w/np.pi
+plot_freq_response(h, w, 'images/freq-response_before.png', fignum=2)
 
 # Poracunaj impulzni odziv filtra
 t, y = signal.impulse((b, a))
@@ -51,15 +52,16 @@ plot_impulse_response(t, y, 'images/impulse-response_before.png', fignum=3)
 new_poles = np.array(poles, copy=True)
 for i, pole in enumerate(poles):
     if np.abs(pole) > 1:
-        print(pole)
         new_poles[i] = 1 / np.conj(pole)
 
 fix_a = np.poly(new_poles)
+print(fix_a)
 zplane(b, fix_a, 'images/zplane_after.png', fignum=4)
 
 # Poracunaj frekvencni odziv popravljenega filtra
 w, h = signal.freqz(b, fix_a)
-plot_freq_response(h, 'images/freq-response_after.png', fignum=5)
+w = w/np.pi
+plot_freq_response(h, w, 'images/freq-response_after.png', fignum=5)
 
 # Poracunaj impulzni odziv popravljenega filtra
 t, y = signal.impulse((b, fix_a))
